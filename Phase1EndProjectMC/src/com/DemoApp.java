734507681e66f90de8b2fceecb1d011f2f247225
MCaldeira <miguelcaldeira@yahoo.com>
2023-09-19 11:41:32 +0000

    Project Start

---
+++ Phase1EndProjectMC/src/com/DemoApp.java
@@ -0,0 +1,20 @@
+package com;
+
+import java.io.File;
+
+public class DemoApp {
+
+	public static void main(String[] args) throws Exception{
+		File ff = new File("/home/migueljcaldeira/Desktop/MC_Phase1Project/Phase1EndProjectMC/Files/");
+//		System.out.println(ff.getPath());
+//		System.out.println(ff.isDirectory());
+//		String allFiles [] = ff.list();
+//		for (String name:allFiles) {
+//			 System.out.println(name);
+//		}
+		String filename = "Mike.txt";
+		ff = new File(ff.getPath()+filename);
+		ff.createNewFile();
+	}
+
+}
