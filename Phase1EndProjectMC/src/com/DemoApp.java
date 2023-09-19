0da3a51b8db8bf376b24c6a9ff7d2971c41ba399
MCaldeira <miguelcaldeira@yahoo.com>
2023-09-19 15:40:32 +0000

    Developing...

--- Phase1EndProjectMC/src/com/DemoApp.java
+++ Phase1EndProjectMC/src/com/DemoApp.java
@@ -1,20 +1,27 @@
 package com;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 public class DemoApp {
 
 	public static void main(String[] args) throws Exception{
-		File ff = new File("/home/migueljcaldeira/Desktop/MC_Phase1Project/Files");
+		File ff = new File("//home/migueljcaldeira/Desktop/MC_Phase1Project/FileFolder");
 //		System.out.println(ff.getPath());
 //		System.out.println(ff.isDirectory());
 //		String allFiles [] = ff.list();
 //		for (String name:allFiles) {
 //			 System.out.println(name);
 //		}
-		String filename = "Mike.txt";
-		ff = new File(ff.getPath()+filename);
+		String fileName = "Barbara.txt";
+		String newPath = ff.getPath()+"//"+fileName;
+//		FileOutputStream fos = new FileOutputStream(newPath);
+//		fos.write('A');
+//		fos.close();
+//		System.out.println("Success...");
+		ff = new File(newPath);
 		ff.createNewFile();
+		
 	}
 
 }
