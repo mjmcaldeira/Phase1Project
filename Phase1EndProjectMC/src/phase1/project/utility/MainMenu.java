5ccf31f73d9748c13991400bf9a1fc4fc38256d9
MCaldeira <miguelcaldeira@yahoo.com>
2023-09-20 11:18:01 +0000

    Main Menu class handling exceptions

--- Phase1EndProjectMC/src/phase1/project/utility/MainMenu.java
+++ Phase1EndProjectMC/src/phase1/project/utility/MainMenu.java
@@ -2,6 +2,7 @@ package phase1.project.utility;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import phase1.project.exception.ProperOptionValue;
@@ -12,46 +13,61 @@ public class MainMenu {
 	
 	public static void menuOptions() {
 		Scanner sc = new Scanner(System.in);
-		String con;
+		String con = null;
 		
 		do {
 			System.out.println("\n1: Display all files in ascending order"+"\n");
 			System.out.println("2: Other options"+"\n");
 			System.out.println("3: Exit"+"\n");
 			System.out.println("Choose one option");
-			int choice = sc.nextInt();
-			switch (choice) {
-			case 1:String listOfFiles [] = rootPath.list();
-				Arrays.sort(listOfFiles);
-				System.out.println("\nFiles in Ascending Order:");
-				for(String name:listOfFiles) {
-					System.out.println(name);
-				}
-				
-				break;
-				
-			case 2:SubMenu.subMenuOptions();
-				
-				break;
-				
-			case 3:System.exit(0);
-				
-				break;
-			
-			}
-			
-			System.out.println("Do you want to continue? (y/n)");
-			con= sc.next();
-			try {
-			if (!con.equals("y") || !con.equals("n")) {
-				 throw new ProperOptionValue("Please enter y or n \n");
-			}
-			}catch(Exception e) {
-				System.out.print(e);
-			}
-		} while (con.equalsIgnoreCase("y"));
-		
-		
-	}
+					
+
+			int choice;
+            try {
+                choice = sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.nextLine(); // Consume the invalid input
+                System.out.println("Invalid choice, please enter a valid option\n");
+                continue; // Continue the loop to ask again for a valid choice
+            }
+
+            switch (choice) {
+                case 1:
+                    String[] listOfFiles = rootPath.list();
+                    Arrays.sort(listOfFiles);
+                    System.out.println("\nFiles in Ascending Order:");
+                    for (String name : listOfFiles) {
+                        System.out.println(name);
+                    }
+
+                    break;
+
+                case 2:
+                    SubMenu.subMenuOptions();
+
+                    break;
+
+                case 3:
+                    System.out.println("Exiting.... Thank you!\n");
+                    System.exit(0);
+
+                    break;
+
+                default:
+                    System.out.println("Invalid choice, please enter a valid option\n");
+            }
+
+            do {
+                System.out.println("Do you want to continue? (y/n)");
+                con = sc.next();
+
+                if (!con.equals("y") && !con.equals("n")) {
+                    System.out.println("\nPlease enter y or n\n");
+                }
+
+            } while (!con.equalsIgnoreCase("y") && !con.equalsIgnoreCase("n"));
+
+        } while (con.equalsIgnoreCase("y"));
+    }
 }
  
